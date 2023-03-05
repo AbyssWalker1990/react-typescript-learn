bb92b305a4ed84549e418f5aacf26737be2fb28c
Vova <bordiian.v@gmail.com>
2023-03-05 13:38:15 +0200

    added form and submitHandler for display input in console for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ToDoList from './components/ToDoList';
-
+import NewToDo from './components/NewToDo';
 
 const App: React.FC = () =>  {
   const todos = [{id: 't1', text: 'Finish the course'}]
   return (
     <div className="App">
-      {/* Component that adds ToDo */}
+      <NewToDo />
       <ToDoList items={todos}/>
     </div>
   );
