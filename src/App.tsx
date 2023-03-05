ca0e698b5fd848af72d0d30d82caef55f150e805
Vova <bordiian.v@gmail.com>
2023-03-05 17:20:00 +0200

    added deleteHandler and changed generation of nique id by adding uuid library

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,32 @@ import React , { useState } from 'react';
 import ToDoList from './components/ToDoList';
 import NewToDo from './components/NewToDo';
 import { Todo } from './todo.models'
+import { v4 as uuidv4 } from 'uuid'
 
 const App: React.FC = () =>  {
   const [todos, setTodos] = useState<Todo[]>([])
 
   const todoAddHandler = (text: string) => {
-    setTodos(prevTodos => [...prevTodos, {id: Math.random.toString(), text: text}])
+    setTodos(prevTodos => [...prevTodos, {id: genId(), text: text}])
+    todos.map(todo => {
+      console.log(todo.id)
+    })
+  }
+
+  const genId = () => {
+    return uuidv4()
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
   }
 
   return (
     <div className="App">
       <NewToDo onAddTodo={todoAddHandler} />
-      <ToDoList items={todos}/>
+      <ToDoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
