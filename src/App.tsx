4a8b641b28820cff0a795ca1434fe7857c02ca76
Vova <bordiian.v@gmail.com>
2023-03-05 14:09:40 +0200

    deal with ts type features for useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React , { useState } from 'react';
 import ToDoList from './components/ToDoList';
 import NewToDo from './components/NewToDo';
+import { Todo } from './todo.models'
 
 const App: React.FC = () =>  {
-  const todos = [{id: 't1', text: 'Finish the course'}]
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const todoAddHandler = (text: string) => {
-    console.log(text)
+    setTodos([{id: Math.random.toString(), text: text}])
   }
 
   return (
